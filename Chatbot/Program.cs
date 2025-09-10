a33feeca0e55b2d5f81165d8fe02ddcce573cbed
beajmercantec <beaj@mercantec.dk>
2025-09-10 08:30:28 +0200

    Add files via upload

---
+++ Chatbot/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Chatbot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Chatbot backend running... (TODO: implement logic)");
+        }
+    }
+}
